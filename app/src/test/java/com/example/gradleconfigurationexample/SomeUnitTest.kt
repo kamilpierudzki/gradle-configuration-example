2894edc2f03114059684c5c069006bd3262378f7
Kamil Pierudzki <kamil.pierudzki@vimncontractor.com>
2020-07-10 12:18:33 +0200

    Add project

---
+++ app/src/test/java/com/example/gradleconfigurationexample/SomeUnitTest.kt
@@ -0,0 +1,11 @@
+package com.example.gradleconfigurationexample
+
+import org.junit.Test
+
+class SomeUnitTest {
+
+    @Test
+    fun `example junit test`() {
+
+    }
+}
---
+++ some-feature/src/main/java/com/example/somefeature/User.kt
@@ -0,0 +1,3 @@
+package com.example.somefeature
+
+class User(val name: String, val surname: String)
